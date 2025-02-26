162e1f0ba73d06eeb79df3fec0a0761ecdb83a74
Ian-spreatics <ian@spreatics.com>
2025-02-26 13:52:48 +0900

    초기 커밋.

---
+++ project1/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//더하기 함수
+int add(int a, int b) {
+	return a + b;
+}
+
+int main() {
+	int result = add(3, 5);
+	int result2 = add(4, 5);
+	cout << result;
+	//return 0;
+}
